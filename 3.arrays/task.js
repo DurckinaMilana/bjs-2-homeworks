b8387e6dcf1a879335bd1ee698d678dbf470dcf6
Milana <durckina.milana2016@yandex.ru>
2022-12-01 23:41:45 +0300

    new com

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
+  result = arr1.length == arr2.length && arr1.every((value, index) => value === arr2[index]); 
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
 
+  const sortArr = arr.filter((number) => number > 0);
+  const sortArr2 = sortArr.filter((number) => number % 3 === 0);
+  resultArr = sortArr2.map((number) => number * 10);
   return resultArr; // array
 }
